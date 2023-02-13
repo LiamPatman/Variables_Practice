318fb092a90546ffdadc5ae5a0c0cc720fc7afbf
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-02-14 11:55:48 +1300

    Final Commit _14th/feb

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,43 @@
+// This content links with 'contentForLoop' file
+
 <template>
   <!-- all HTML content -->
   <!-- other components also are used in template area-->
-  <h1>This is plain text in h1</h1>
-  <h2>{{ myMessage }}</h2>
-  <h2>Plain text combined with variable value {{ myMessage }}</h2>
-
-  <!-- here is how we combine it inside a message -->
-  <h3>Price is : {{ price }}$</h3>
-
-  <!-- v-if is the way we can do display:none depending on a boolean -->
-  <p v-if="myBoolean">This message displays only if myBoolean is true</p>
-
-  <!-- we can provide a variable as property to a html (such as class, src, href etc) -->
-  <!-- the way to do it is by writing v-bind: before the property, but since it is used often, a shorter version was introduced later as just :   -->
-  <!-- we can use regular class and v-bind:class together in same element -->
-  <span v-bind:class="myColor" class="bordered">colored</span>
-  <span :class="myColor">colored</span>
-
-
+  <ContentVue v-for="content in contents_array" :content_obj="content" />
 </template>
-
-
 <style scoped>
   /* all CSS classes are defined here */
-  .red{color: red;}
-  .blue{color: blue;}
-  .bordered{border: 1px solid black;}
-  .binded-color{
-    color: v-bind(myColor);
-  }
 </style>
-
-
 <script setup>
   // all imports for other components are done in setup
   // Prop declarations also are done in script setup
+  import ContentVue from './components/Content.vue'
 </script>
-
-
 <script>
 export default {
   data(){
-    return{
-      // this is where we declare all variables
-      // format is like below
-      // variableName : 'value'
-      // example below
-      // myBoolean : true,
-      // myMessage : 'hello world',
-      // we separate variables with comma ,
-      myMessage : 'Hello World',
-      price : 10,
-      myBoolean : true,
-      myColor : 'red'
-    }
+    return{ // data Start
+      contents_array:[
+        {
+          title:'title one',
+          content:'content one',
+          content_revealed:true,
+          approved:true
+        },
+        {
+          title:'title two',
+          content:'content two',
+          content_revealed:true,
+          approved:false
+        },
+        {
+          title:'title three',
+          content:'content three',
+          content_revealed:false,
+          approved:true
+        }
+      ]
+    } // data End
   }
 }
 </script>
---
+++ src/components/contentForLoop.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="content-div" :class="{approvedCSS:content_obj.approved}">
+        <h2>{{ content_obj.title }}</h2>
+        <p v-if="content_obj.content_revealed">{{ content_obj.content }}</p>
+    </div>
+</template>
+<style scoped>
+  /* all CSS classes are defined here */
+  .content-div{
+    border: 1px solid black;
+    padding: 10px;
+    margin: 5px;
+  }
+  .approvedCSS{background-color: limegreen;}
+</style>
+<script setup>
+defineProps({
+  content_obj: {
+    type: Object,
+    required: true
+  }
+});
+</script>
