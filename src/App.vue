b88bcb791ede6f9de1c91ca0553418c4fdaf8489
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-02-24 11:16:28 +1300

    24/02 later morning

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,49 @@
 <script setup>
   import HeaderComponent from './components/Header.vue'
   import SearchComponent from './components/SearchBar.vue'
+  import NewsArticleComponent from './components/NewsArticle.vue';
 </script>
 
 <template>
 <!-- <span @click="data_fetch">LOAD</span> -->
   <HeaderComponent />
   <SearchComponent />
-  <h2>placeholder</h2>
+  <NewsArticleComponent />
   
+  <!-- <div class="article" v-for='news_article in my_data_array'>
+    <h1>{{news_article.title}}</h1>
+    <h2>{{news_article.author}}</h2>
+    <h3>{{news_article.description}}</h3>
+  </div> -->
 
-<!-- <h3 v-for='news_article in my_data_array'>{{news_article.title}}</h3> -->
 </template>
 
 
 <script>
-export default {
-  data(){ // data itself starts
-    return{ // data variables starts
-      my_data_array:[]
-    } // data variables end
-  }, // data itself ends
-  methods:{ // funtions are written in methods
-    async data_fetch(){
-      const response = await fetch("https://newsapi.org/v2/everything?q=tesla&apiKey=ffd711aad23843739f3a3c42ff9b8f7d");
-      const received_data = await response.json();
-      // at this point our data is available under received_data variable
+// export default {
+//   data(){ // data itself starts
+//     return{ // data variables starts
+//       my_data_array:[]
+//     } // data variables end
+//   }, // data itself ends
+//   methods:{ // funtions are written in methods
+//     async data_fetch(){
+//       const response = await fetch("https://newsapi.org/v2/everything?q=tesla&apiKey=ffd711aad23843739f3a3c42ff9b8f7d");
+//       const received_data = await response.json();
+//       // at this point our data is available under received_data variable
       
-      this.my_data_array = received_data.articles;
+//       this.my_data_array = received_data.articles;
 
-      console.log( this.my_data_array[0] );
-    }
-  }, // methods end
-  created(){ // created() is where initial code runs once
-    this.data_fetch();
-  } // end of created
-}
+//       console.log( this.my_data_array[0] );
+//     }
+//   }, // methods end
+//   created(){ // created() is where initial code runs once
+//     this.data_fetch();
+//   } // end of created
+// }
 </script>
 
 
 <style>
+  
 </style>
--- src/components/NewsArticle.vue
+++ src/components/NewsArticle.vue
@@ -1,38 +1,42 @@
 <template>
-    <div class="news-article">
-
-
-
-    </div>
-</template>
-
-<style scoped>
-.news-article{
-
-}
-.news-thumb{
-
-}
-.content{
     
-}
-</style>
-
-<script setup>
-defineProps({
-    NewsArticleObj: {
-        type: Object,
-        required: true
-    }
-});
-</script>
+      
+      <div class="article" v-for='news_article in my_data_array'>
+        <h1>{{news_article.title}}</h1>
+        <h2>{{news_article.author}}</h2>
+        <h3>{{news_article.description}}</h3>
+      </div>
+    
+</template>
 
 <script>
 export default {
-    data() {
-        return{
-            content_reveal : false
-        }
-    },
+  data(){ // data itself starts
+    return{ // data variables starts
+      my_data_array:[]
+    } // data variables end
+  }, // data itself ends
+  methods:{ // funtions are written in methods
+    async data_fetch(){
+      const response = await fetch("https://newsapi.org/v2/everything?q=tesla&apiKey=ffd711aad23843739f3a3c42ff9b8f7d");
+      const received_data = await response.json();
+      // at this point our data is available under received_data variable
+      
+      this.my_data_array = received_data.articles;
+
+      console.log( this.my_data_array[0] );
+    }
+  }, // methods end
+  created(){ // created() is where initial code runs once
+    this.data_fetch();
+  } // end of created
 }
 </script>
+
+<style scoped>
+  .article{
+    background-color: cadetblue;
+    color: aliceblue;
+    padding: 10px;
+  }
+</style>
