3c0314ac3baa92628325333751cfbfedb6f5bd20
LiamPatman <270025210@yoobeestudent.ac.nz>
2023-02-24 09:42:35 +1300

    24/02 Morning

--- src/App.vue
+++ src/App.vue
@@ -8,33 +8,34 @@
   <HeaderComponent />
   <SearchComponent />
   <h2>placeholder</h2>
+  
 
 <!-- <h3 v-for='news_article in my_data_array'>{{news_article.title}}</h3> -->
 </template>
 
 
 <script>
-// export default {
-//   data(){ // data itself starts
-//     return{ // data variables starts
-//       my_data_array:[]
-//     } // data variables end
-//   }, // data itself ends
-//   methods:{ // funtions are written in methods
-//     async data_fetch(){
-//       const response = await fetch("https://newsapi.org/v2/everything?q=tesla&apiKey=ffd711aad23843739f3a3c42ff9b8f7d");
-//       const received_data = await response.json();
-//       // at this point our data is available under received_data variable
+export default {
+  data(){ // data itself starts
+    return{ // data variables starts
+      my_data_array:[]
+    } // data variables end
+  }, // data itself ends
+  methods:{ // funtions are written in methods
+    async data_fetch(){
+      const response = await fetch("https://newsapi.org/v2/everything?q=tesla&apiKey=ffd711aad23843739f3a3c42ff9b8f7d");
+      const received_data = await response.json();
+      // at this point our data is available under received_data variable
       
-//       this.my_data_array = received_data.articles;
+      this.my_data_array = received_data.articles;
 
-//       console.log( this.my_data_array[0] );
-//     }
-//   }, // methods end
-//   created(){ // created() is where initial code runs once
-//     this.data_fetch();
-//   } // end of created
-// }
+      console.log( this.my_data_array[0] );
+    }
+  }, // methods end
+  created(){ // created() is where initial code runs once
+    this.data_fetch();
+  } // end of created
+}
 </script>
 
 
